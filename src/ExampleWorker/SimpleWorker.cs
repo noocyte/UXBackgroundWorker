2f43a418a9bb34aaeb2e89c5cc7224c80e958038
noocyte <jarle.nygaard@outlook.com>
2013-10-15 19:31:59 +0200

    Adding example

---
+++ src/ExampleWorker/SimpleWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UXBackgroundWorker;
+
+namespace ExampleWorker
+{
+    public class SimpleWorker:BaseWorker
+    {
+        protected override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
