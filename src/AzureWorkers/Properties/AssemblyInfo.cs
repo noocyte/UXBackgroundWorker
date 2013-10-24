f0d7a806e314a8b70e5a5605721f331ebc75069a
noocyte <jarle.nygaard@outlook.com>
2013-10-24 08:37:18 +0200

    Updated

--- src/AzureWorkers/Properties/AssemblyInfo.cs
+++ src/AzureWorkers/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UXBackgroundWorker")]
+[assembly: AssemblyTitle("Proactima Azure Workers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UXBackgroundWorker")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Proactima Azure Workers")]
+[assembly: AssemblyCopyright("Copyright © Proactima Soluitions AS - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
