5ceab2f441432ec11272520c9ccfe519cd243819
Jarle Nygård <jarle.nygaard@outlook.com>
2014-04-04 08:32:24 +0200

    Delete buildpack.cmd

--- buildpack.cmd
+++
@@ -1,6 +0,0 @@
-mkdir release
-
-@call msbuild src\AzureWorkers\AzureWorkers.csproj /t:clean
-@call msbuild src\AzureWorkers\AzureWorkers.csproj /p:Configuration=Release
-
-src\.nuget\NuGet.exe pack -sym src\AzureWorkers\AzureWorkers.csproj -OutputDirectory release
