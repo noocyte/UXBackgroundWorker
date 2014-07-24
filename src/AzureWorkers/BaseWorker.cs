fb753ad87692c92834569eda3e502ed89d25fb3c
noocyte <jarle.nygaard@outlook.com>
2014-07-24 12:02:07 +0200

    Better handling of exceptions

--- src/AzureWorkers/BaseWorker.cs
+++ src/AzureWorkers/BaseWorker.cs
@@ -7,37 +7,51 @@ namespace Proactima.AzureWorkers
     public abstract class BaseWorker
     {
         private CancellationTokenSource _cancellationTokenSource;
-        protected CancellationToken Token { get; set; }
+        protected CancellationToken Token { get; private set; }
 
+        /// <summary>
+        /// Set to false to disable the worker.
+        /// Default is true.
+        /// </summary>
         public virtual bool Enabled
         {
             get { return true; }
         }
 
+        /// <summary>
+        /// The wait time before calling 'Start' when the worker exits the 'Start' method
+        /// Expressed in millisecond, default is 1000ms (1 second).
+        /// </summary>
         protected virtual int LoopWaitTime
         {
             get { return 1000; }
         }
 
+        /// <summary>
+        /// The method to do actual work in a Worker.
+        /// If method exits, it will be called again after LoopWaitTime passes.
+        /// </summary>
+        /// <returns></returns>
         protected abstract Task StartAsync();
 
         internal async Task ProtectedRun(CancellationTokenSource tokenSource)
         {
-            try
-            {
-                _cancellationTokenSource = tokenSource;
-                Token = _cancellationTokenSource.Token;
+            _cancellationTokenSource = tokenSource;
+            Token = _cancellationTokenSource.Token;
 
-                while (!Token.IsCancellationRequested)
+            while (!Token.IsCancellationRequested)
+            {
+                try
                 {
                     await StartAsync().ConfigureAwait(false);
-                    if (LoopWaitTime > 0)
-                        Token.WaitHandle.WaitOne(LoopWaitTime);
                 }
-            }
-            catch (Exception exception)
-            {
-                ErrorLogging("An exception was caught in BaseWorker.ProtectedRun", exception);
+                catch (Exception exception)
+                {
+                    ErrorLogging("An exception was caught in BaseWorker.ProtectedRun", exception);
+                }
+
+                if (LoopWaitTime > 0)
+                    Token.WaitHandle.WaitOne(LoopWaitTime);
             }
         }
 
@@ -47,18 +61,40 @@ namespace Proactima.AzureWorkers
                 _cancellationTokenSource.Cancel();
         }
 
+        /// <summary>
+        /// Basic error logging
+        /// </summary>
+        /// <param name="message">Message provided by the worker</param>
+        /// <param name="ex">The current exception</param>
         protected virtual void ErrorLogging(string message, Exception ex = null)
         {
         }
 
+        /// <summary>
+        /// Basic error logging with a message Id
+        /// </summary>
+        /// <param name="message">Message provided by the worker</param>
+        /// <param name="messageId">Message Id (correlation id)</param>
+        /// <param name="ex">The current exception</param>
         protected virtual void ErrorLogging(string message, string messageId = "", Exception ex = null)
         {
         }
 
+        /// <summary>
+        /// Informational logging from the worker
+        /// </summary>
+        /// <param name="message">Message provided by the worker</param>
+        /// <param name="messageId">Message Id (correlation id)</param>
         protected virtual void InfoLogging(string message, string messageId = "")
         {
         }
 
+        /// <summary>
+        /// Debug logging from the worker
+        /// </summary>
+        /// <param name="message">Message provided by the worker</param>
+        /// <param name="messageId">Message Id (correlation id)</param>
+        /// <param name="timerValue">Timings</param>
         protected virtual void DebugLogging(string message, string messageId = "", double timerValue = 0.0)
         {
         }
--- src/ExampleWorker/SimpleWorker.cs
+++ src/ExampleWorker/SimpleWorker.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Proactima.AzureWorkers;
 
@@ -9,10 +11,28 @@ namespace ExampleWorker
     {
         protected override async Task StartAsync()
         {
+            var rnd = new Random();
+            await Task.Delay(rnd.Next(3000)).ConfigureAwait(false);
+            
+            var r = rnd.Next(5);
+
+            if (r == 1)
+                throw new InvalidOperationException("Some random exception!");
+            InfoLogging("No exception!");
             var client = new HttpClient();
 
             if (!Token.IsCancellationRequested)
                 await client.GetAsync("http://blog.noocyte.net").ConfigureAwait(false);
+        }
+
+        protected override void InfoLogging(string message, string messageId = "")
+        {
+            Debug.WriteLine(message);
+        }
+
+        protected override void ErrorLogging(string message, Exception ex = null)
+        {
+            Debug.WriteLine(message);
         }
     }
 }
